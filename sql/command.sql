add20266b3b8fe5f199284bc7fbfc804abb7ba87
hellycia <wattiaux.helene@hotmail.fr>
2020-08-13 14:33:21 +0200

    All but not the last

---
+++ sql/command.sql
@@ -0,0 +1,94 @@
+-- Récupérer le numéro de commande et la date de toutes
+-- les commandestriées par date croissante:
+SELECT id, date
+FROM `order`
+ORDER BY date;
+
+-- Récupérer le numéro de commande, le prénom
+-- et le nom de l’utilisateur qui a  commandé, la date de la commande:
+SELECT `order`.id, firstname, lastname, date
+FROM user
+JOIN `order` ON `order`.user_id = user.id;
+
+
+-- Récupérer le numéro de commande, le prénom et le nom de l’utilisateur qui a  commandé,
+-- la date de la commande et le montant total de la commande par date croissante.
+SELECT `order`.id, firstname, lastname, date,
+SUM(order_line.price*order_line.quantity) AS Montant_total
+FROM user
+JOIN `order` ON `order`.user_id = user.id
+JOIN order_line ON `order`.id = order_line.order_id
+GROUP BY order_id
+ORDER BY date;
+
+
+-- Récupérer le prénom et le  nom des 3 personnes ayant commandé pour le plus
+-- (= qui nous a rapporté le plus de sous), montant total commandé.
+SELECT firstname, lastname, SUM(order_line.price*order_line.quantity) AS Montant_total
+FROM user
+JOIN `order` ON `order`.user_id = user.id
+JOIN order_line ON `order`.id = order_line.order_id
+GROUP BY order_id
+ORDER BY Montant_total desc limit 3;
+
+
+-- Avoir par produit (titre du produit), combien de fois un produit a été commandé
+-- (par commande différente et par qtt totale)
+SELECT title, SUM(order_line.quantity) AS Quantité_total, COUNT(order_id) AS Commande_total
+FROM product
+JOIN order_line ON product.id = order_line.product_id
+GROUP BY product_id;
+
+
+-- Avoir l'ID, nom et prénom des clients qui n'ont rien commandé
+SELECT user.id, firstname, lastname
+FROM user
+LEFT JOIN `order` ON `order`.user_id = user.id
+WHERE `order`.user_id IS NULL;
+
+
+-- Savoir quel est le produit le plus commandé par les femmes
+SELECT title, SUM(order_line.quantity) AS Quantité
+FROM product
+JOIN order_line ON product.id = order_line.product_id
+JOIN `order` ON `order`.id = order_line.order_id
+JOIN user ON user.id = `order`.user_id
+WHERE user.civil = 'Femme'
+GROUP BY product_id
+ORDER BY Quantité desc limit 1;
+
+
+-- Savoir quel produit est le plus commandé l'hiver
+SELECT title, SUM(order_line.quantity) AS Quantité
+FROM product
+JOIN order_line ON product.id = order_line.product_id
+JOIN `order` ON `order`.id = order_line.order_id
+WHERE `order`.date BETWEEN '2019-12-20' AND '2020-02-20'
+GROUP BY product_id
+ORDER BY Quantité desc limit 1;
+
+
+-- Donner un bon de réduction de 5 euros pour tout client qui a commandé plus de 20 euros
+-- pseudo code: insert into coupon un coupn where montant_commande > 20
+
+INSERT INTO coupon (user_id, price, quantity)
+VALUES (
+    (SELECT user.id, SUM(order_line.price*order_line.quantity) AS Montant_total
+    FROM user
+    JOIN `order` ON `order`.user_id = user.id
+    JOIN order_line ON order_line.order_id = `order`.id
+    GROUP BY order_id
+    WHERE Montant_total > 20
+), 5, 1);
+
+-- ajouter un coupon
+INSERT INTO coupon (price, quantity)
+VALUES (5, 1);
+
+-- recuperer les users qui on des commandes de plus de 20e
+SELECT firstname, lastname, SUM(order_line.price*order_line.quantity) AS Montant_total
+FROM user
+JOIN `order` ON `order`.user_id = user.id
+JOIN order_line ON `order`.id = order_line.order_id
+GROUP BY order_id
+WHERE Montant_total > 20;
---
+++ sql/insertion.sql
@@ -0,0 +1,33 @@
+INSERT INTO user (firstname, lastname, age, civil) VALUES
+('Jean', 'Moulin', 45, 'Homme'),
+('Marine', 'Gautier', 30, 'Femme'),
+('Carine', 'Tellier', 38, 'Femme'),
+('Marc', 'Jolier', 29, 'Homme'),
+('Didier', 'Riou', 60, 'Homme');
+
+INSERT INTO product (price, title) VALUES
+(29.99, 'chapeau'),
+(9.99, 'chaussette'),
+(159, 'manteau'),
+(15.45, 'poster'),
+(89.99, 'sweat');
+
+INSERT INTO `order` (id, user_id, date) VALUES
+(1, (SELECT id FROM user WHERE firstname='Jean'), '2020-01-01 20:30:00'),
+(2, (SELECT id FROM user WHERE firstname='Jean'), '2020-07-01 12:40:00'),
+(3, (SELECT id FROM user WHERE firstname='Carine'), '2020-02-10 10:21:23'),
+(4, (SELECT id FROM user WHERE firstname='Marine'), NOW() ),
+(5, (SELECT id FROM user WHERE firstname='Carine'), '2019-10-03 22:45:12'),
+(6, (SELECT id FROM user WHERE firstname='Carine'), '2019-12-03 22:45:12'),
+(7, (SELECT id FROM user WHERE firstname='Carine'), '2019-12-25 22:45:12');
+
+
+INSERT INTO order_line (order_id, product_id, price, quantity) VALUES
+(1, ( SELECT id FROM product WHERE title='chapeau'), 29.99, 1 ),
+(1, ( SELECT id FROM product WHERE title='poster'), 15.45, 2 ),
+(2, (SELECT id FROM product WHERE title='poster'), 15.45, 1 ),
+(3, (SELECT id FROM product WHERE title='sweat'), 89.99, 1 ),
+(4, (SELECT id FROM product WHERE title='poster'), 15.45, 10 ),
+(5, (SELECT id FROM product WHERE title='chaussette'), 9.99, 1 ),
+(6, (SELECT id FROM product WHERE title='sweat'), 89.99, 5 ),
+(7, (SELECT id FROM product WHERE title='chaussette'), 89.99, 8 );
